2dc946ff2f7e09c4a916bb87f2b86188ca499f50
RDOToole89 <roibinotoole@gmail.com>
2020-12-26 14:39:22 +0100

    Create GraphQL RootQuery

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -9,7 +10,12 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
-app.use('/graphql', graphqlHTTP({}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+  })
+);
 
 app.get('/', (req, res, next) => {
   res.json('Hello World');
--- schema/schema.js
+++ schema/schema.js
@@ -1,7 +1,9 @@
 const graphql = require('graphql');
+const { argsToArgsConfig } = require('graphql/type/definition');
 
-const { GraphQLObectType, GraphQLString } = graphql;
+const { GraphQLObectType, GraphQLString, GraphQLSchema } = graphql;
 
+// Schema Type that defines data object type which defines "Book"
 const BookType = new GraphQLObectType({
   name: 'Book',
   fields: () => ({
@@ -9,4 +11,33 @@ const BookType = new GraphQLObectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
+});
+
+// Schema Type that defines how you will query a GraphQLData or jump into the Graph
+const RootQuery = new GraphQLObectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      // Defines the type of data
+      type: BookType,
+      // args is the identifier that we expect to receive in order to fullfull a query
+      // in this case an id which is a string.
+      // this data will fire the resolve function which will deliver our data
+      args: { id: { type: GraphQLString } },
+      // code to get data from db / other source
+      resolve(parent, args) {},
+    },
+  },
+});
+
+// Example frontend query
+// book(id: "123") {
+//   name
+//   genre
+// }
+
+// We define a new GraphQL Schema and passing in options defining which query the user
+// can use to retreive data from the frontend
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
