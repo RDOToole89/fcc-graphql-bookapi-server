4d7dc5259b58d8a3faed646ada6044eba51546be
RDOToole89 <roibinotoole@gmail.com>
2020-12-26 14:17:45 +0100

    Install and setup graphql / graphql-express

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -8,8 +9,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
+app.use('/graphql', graphqlHTTP({}));
+
 app.get('/', (req, res, next) => {
   res.json('Hello World');
 });
 
-server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
